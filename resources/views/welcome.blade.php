c4e67a87e6b94175f0599facb706d2571c812938
ninecolors <76292655@qq.com>
2018-10-06 09:06:18 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hollo Laravel! - by Summer
+                    Hollo Laravel! - by Panjingkai
                 </div>
 
                 <div class="links">
